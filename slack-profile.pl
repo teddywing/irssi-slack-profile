aa961a95449d5be78a11df3e9531ff4cad2aa582
Teddy Wing <git@teddywing.com>
2016-09-10 03:38:41 -0400

    Move HTTP code to a subroutine
    File this code away into its own container so we can work on some JSON
    decoding.

--- slack-profile.pl
+++ slack-profile.pl
@@ -4,20 +4,22 @@ use LWP::UserAgent;
 use HTTP::Request::Common;
 use Mozilla::CA;
 
-my $ua = LWP::UserAgent->new;
-$ua->agent('Mozilla/5.0');
+sub users_list {
+	my $ua = LWP::UserAgent->new;
+	$ua->agent('Mozilla/5.0');
 
-my $req = HTTP::Request->new(GET => 'http://ip.jsontest.com/');
+	my $req = HTTP::Request->new(GET => 'http://ip.jsontest.com/');
 
-$req->header('content-type' => 'application/json');
+	$req->header('content-type' => 'application/json');
 
-my $resp = $ua->request($req);
+	my $resp = $ua->request($req);
 
-if ($resp->is_success) {
-	my $message = $resp->decoded_content;
-	print $message;
-}
-else {
-	print $resp->code . "\n";
-	print $resp->message . "\n";
+	if ($resp->is_success) {
+		my $message = $resp->decoded_content;
+		print $message;
+	}
+	else {
+		print $resp->code . "\n";
+		print $resp->message . "\n";
+	}
 }
