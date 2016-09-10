cd3984a354b00f638493f6a7099370ec67e9f706
Teddy Wing <git@teddywing.com>
2016-09-10 16:46:59 -0400

    Implement binary search to find users by username
    Create a `binary_search` subroutine that will allow us to search through
    an array.
    
    Used Wikipedia and Rosetta Code as a reference:
      - https://en.wikipedia.org/wiki/Binary_search_algorithm#Algorithm
      - http://rosettacode.org/wiki/Binary_search#Perl
    
    In order to test matches, `binary_search` expects an anonymous function
    that will return 0, 1, or -1 depending on the result of the comparison.
    
    The anonymous function we use compares usernames alphabetically to the
    one we want to find.
    
    Finally, we have a section commented out that performs a linear search
    for a user in the members array. We will time this and the binary search
    version to see how they compare. Slack attests that the `users.list` API
    method provides users "in no particular order"
    (https://api.slack.com/methods/users.list), however most users appear to
    be ordered by username. The one exception appears to be 'slackbot', who
    appears at the end of the list.

--- slack-profile.pl
+++ slack-profile.pl
@@ -1,9 +1,14 @@
 use strict;
 
+use 5.010;
+
+use Data::Dumper;
+
 use JSON;
 use LWP::UserAgent;
 use HTTP::Request::Common;
 use Mozilla::CA;
+use POSIX;
 
 sub users_list {
 	my $ua = LWP::UserAgent->new;
@@ -26,10 +31,57 @@ sub users_list {
 }
 
 
+sub binary_search {
+	my ($array, $match_func) = @_;
+	my $left = 0;
+	my $right = scalar @{$array} - 1;
+
+	while ($left <= $right) {
+		my $middle = floor(($left + $right) / 2);
+
+		my $equality = $match_func->($array->[$middle]);
+
+		if ($equality == 0) {
+			return $array->[$middle];
+		}
+		elsif ($equality == -1) {
+			$left = $middle + 1;
+		}
+		elsif ($equality == 1) {
+			$right = $middle - 1;
+		}
+	}
+
+	return -1;
+}
+
 open(my $fh, '<', 'users.list.json');
 {
 	local $/;
 	my $json_text = <$fh>;
 	my $users_list = decode_json($json_text);
+	my @members = @{$users_list->{'members'}};
+
+	my $username = 'teddy';
+	my $user = binary_search($users_list->{'members'}, (sub {
+		my ($user) = @_;
+
+		# say Dumper($user);
+		say $user->{'name'};
+		if ($username eq $user->{'name'}) { return 0; }
+		elsif ($username gt $user->{'name'}) { return -1; }
+		else { return 1; }
+	}));
+
+	say Dumper($user);
+	say $user->{'name'};
+
+
+	# for my $user (@members) {
+	# 	if ($user->{'name'} eq 'slackbot') {
+	# 		say Dumper($user);
+	# 		last;
+	# 	}
+	# }
 }
 close $fh;
